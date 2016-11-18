4afc84fe9c1d6935f986dfee7a9dd2f3cabe60f2
JDHL <glinkx8j@gmail.com>
2016-11-18 18:37:03 +0000

    Simplified paintbrush
    Removed dependency on connected and serve-static.
    It is a very basic version, all we have now is just a canvas over the
    whole page and people can draw on it.

--- paintbrush/server.js
+++ paintbrush/server.js
@@ -1,18 +1,29 @@
-(function() {
-  var io;
-  var connect = require('connect');
-  var serveStatic = require('serve-static');
-  var app = connect();
-  app.use(serveStatic(__dirname));
-  app.listen(8080);
-  io = require('socket.io').listen(4000);
-  io.sockets.on('connection', function(socket) {
-    socket.on('drawClick', function(data) {
-      socket.broadcast.emit('draw', {
-        x: data.x,
-        y: data.y,
-        type: data.type
-      });
-    });
-  });
-}).call(this);
+var express = require('express'), 
+    app = express(),
+    http = require('http')
+     ;
+
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+server.listen(8080);
+app.use(express.static(__dirname + '/public'));
+console.log("Server running on 127.0.0.1:8080");
+
+var line_history = [];
+
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // first send the history to the new client
+   for (var i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history 
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+});
