380dda5535460cc8ebf8faefe638513f256d8176
Dylan Lawrence <2137900l@bo720-2-01u.dcs.gla.ac.uk>
2016-11-18 16:04:19 +0000

    Updated CSS Example (cross-browser issues and top bar)

--- CSS Example/script.js
+++ CSS Example/script.js
@@ -9,21 +9,27 @@ function init(){
     click_selector_tool();
 }
 
+
+function click_new(){
+    alert("You clicked new");
+}
+function click_download(){
+    alert("You clicked download");
+}
+function click_share(){
+    alert("Share this link with others:\nhttp://seifubhwel.com/?id=sguihg");
+}
 function click_selector_tool(){
     document.getElementById("selector_icon_div").style.backgroundColor = "lightblue";
-    document.getElementById("selector_text_div").style.backgroundColor = "lightblue";
     if(currently_selected_tool == tool_enum.DRAWTOOL){
         document.getElementById("drawtool_icon_div").style.backgroundColor = "white";
-        document.getElementById("drawtool_text_div").style.backgroundColor = "white";
     }
     currently_selected_tool = tool_enum.SELECTOR
 }
 function click_draw_tool(){
     document.getElementById("drawtool_icon_div").style.backgroundColor = "lightblue";
-    document.getElementById("drawtool_text_div").style.backgroundColor = "lightblue";
     if(currently_selected_tool == tool_enum.SELECTOR){
         document.getElementById("selector_icon_div").style.backgroundColor = "white";
-        document.getElementById("selector_text_div").style.backgroundColor = "white";
     }
     currently_selected_tool = tool_enum.DRAWTOOL
 }
