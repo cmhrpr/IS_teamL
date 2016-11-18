a34161b4953a5e98ca03f52d5a6051de3986f81c
JDHL <glinkx8j@gmail.com>
2016-11-18 23:50:29 +0000

    Added Download to png for Canvas
    Ran into too much trouble with downloading in pdf, so I opted for
    downloading image files instead...

--- paintbrush/public/client.js
+++ paintbrush/public/client.js
@@ -49,3 +49,16 @@ document.addEventListener("DOMContentLoaded", function() {
    }
    mainLoop();
 });
+var canvas = document.getElementById('drawing');
+var button = document.getElementById('download');
+function dlCanvas() {
+	  var dt = canvas.toDataURL('image/png');
+	  /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+	  dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+	  /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+	  dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+	  this.href = dt;
+};
+document.getElementById("dl").addEventListener('click', dlCanvas, false);
