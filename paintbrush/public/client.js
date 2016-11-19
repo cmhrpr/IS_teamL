5bffe3f87b63ef6b95648840dcd5280af8d17ec5
JDHL <glinkx8j@gmail.com>
2016-11-19 14:59:21 +0000

    Added color palette, download, clear and size
    It is now possible to draw in different colors with different brush
    thickness.
    Canvas can be downloaded as a PNG. (Initially wanted to make it into a
    pdf, but encountered too many problems.)
    Canvas can be cleared with one button now.
    The number of users displayed needs to refresh everytime for the numbers
    to be correct.

--- paintbrush/public/client.js
+++ paintbrush/public/client.js
@@ -11,6 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
    var width   = window.innerWidth;
    var height  = window.innerHeight;
    var socket  = io.connect();
+   var $color =  document.getElementById('color');
+   var $size  = document.getElementById('size');
+   var $clear = document.getElementById('clear'); 
+   var $count = document.getElementById('count');
+   
+   
 
    // set canvas to full browser width/height
    canvas.width = width;
@@ -19,6 +25,10 @@ document.addEventListener("DOMContentLoaded", function() {
    // register mouse event handlers
    canvas.onmousedown = function(e){ mouse.click = true; };
    canvas.onmouseup = function(e){ mouse.click = false; };
+   
+   // get the starting size
+	size = $size.options[$size.selectedIndex].value;
+	color = $color.options[$color.selectedIndex].value.toLowerCase();
 
    canvas.onmousemove = function(e) {
       // normalize mouse position to range 0.0 - 1.0
@@ -26,22 +36,52 @@ document.addEventListener("DOMContentLoaded", function() {
       mouse.pos.y = e.clientY / height;
       mouse.move = true;
    };
+   
+   $size.addEventListener('change', function(e) {
+		size = $size.options[$size.selectedIndex].value
+		touchdown = false
+	}, false)
+
+	$color.addEventListener('change', function(e) {
+		color = $color.options[$color.selectedIndex].value.toLowerCase()
+		touchdown = false
+	}, false)
+   
+   $clear.addEventListener('click', function(e) {
+		clearScreen()
+		socket.emit('clear');
+	}, false)
+
 
    // draw line received from server
 	socket.on('draw_line', function (data) {
       var line = data.line;
       context.beginPath();
+	  context.lineWidth = line[2];
+	  context.strokeStyle = line[3];
       context.moveTo(line[0].x * width, line[0].y * height);
       context.lineTo(line[1].x * width, line[1].y * height);
       context.stroke();
    });
+   
+   socket.on('clear', function () {
+      clearScreen()
+   });
+   
+   socket.on('message', function(data){
+	   $count.innerHTML = 'Current Users: ' + data.count;
+   });
+   
+   function clearScreen() {
+		context.clearRect(0,0,width,height)
+	}
    
    // main loop, running every 25ms
    function mainLoop() {
       // check if the user is drawing
       if (mouse.click && mouse.move && mouse.pos_prev) {
          // send line to to the server
-         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev, size, color] });
          mouse.move = false;
       }
       mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
@@ -49,9 +89,11 @@ document.addEventListener("DOMContentLoaded", function() {
    }
    mainLoop();
 });
-var canvas = document.getElementById('drawing');
-var button = document.getElementById('download');
+
+
+
 function dlCanvas() {
+	  var canvas = document.getElementById('drawing');
 	  var dt = canvas.toDataURL('image/png');
 	  /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
 	  dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
@@ -61,4 +103,8 @@ function dlCanvas() {
 
 	  this.href = dt;
 };
-document.getElementById("dl").addEventListener('click', dlCanvas, false);
+window.onload=function(){
+	document.getElementById("dl").addEventListener('click', dlCanvas, false);
+}
+	
+
--- paintbrush/server.js
+++ paintbrush/server.js
@@ -10,15 +10,22 @@ app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
 
 var line_history = [];
+var count = 0;
 
 // event-handler for new incoming connections
 io.on('connection', function (socket) {
+	
+	count++;
 
    // first send the history to the new client
    for (var i in line_history) {
       socket.emit('draw_line', { line: line_history[i] } );
    }
-
+   
+   // report the number of users has increased
+   socket.emit('message',{
+	   count: count
+   });
    // add handler for message type "draw_line".
    socket.on('draw_line', function (data) {
       // add received line to history 
@@ -26,4 +33,16 @@ io.on('connection', function (socket) {
       // send line to all clients
       io.emit('draw_line', { line: data.line });
    });
+   
+   socket.on('clear', function() {
+        line_history = [];
+       io.emit('clear');
+    });
+	
+	socket.on('disconnect', function(){
+		count--;
+    });
+	
+   
+   
 });
