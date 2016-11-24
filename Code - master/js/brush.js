d0e7dd68ab4d8247e7564034b91b733d073e47f9
MariusVin <m.vinickis@gmail.com>
2016-11-24 21:15:27 +0000

    Added functionality for paintbrush (size, color), storing the element data in JSON

--- Code - master/js/brush.js
+++ Code - master/js/brush.js
@@ -1,27 +1,65 @@
-$(document).ready(function(){
-    var moving = false;
-
-    var isDragging = false;
-    $("#canvas").mousemove(function() {
-        if (moving) {
-            var msg = "Handler for .mousemove() called at ";
-            console.log(event.pageX + ", " + event.pageY);
-            var x = event.clientX;     // Get the horizontal coordinate
-            var y = event.clientY;     // Get the vertical coordinate
-            $("#canvas").append("<div class='circle' style='position: absolute; top: "+(y- 130)+"px; left: "+(x-310)+"px; background-color:blue; border-radius: 50px; width: 20px; height: 20px'></div>");
+$( function() {
+    var handle = $( "#custom-handle" );
+    $( "#slider" ).slider({
+        value: '20',
+        create: function() {
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value );
         }
-
     });
+} );
 
-    $("#canvas").mousedown(function() {
-        moving = true;
+$(document).ready(function(){
 
-    })
+    // ----------------------------------PAINTBRUSH--------------------------------------
+    var moving = false;
+    var x;
+    var y;
+    var beid = 0; // brushElement ID
+    var did; // dot ID
+    var color;
+    var size;
+    var brushElements = {};
+    var currentElement;
+    $("#canvas")
+        .mousemove(function() {
+            if (moving) {
+                //var msg = "Handler for .mousemove() called at ";
 
-    $("#canvas").mouseup(function() {
-        moving = false;
+                x = event.clientX;     // Get the horizontal coordinate
+                y = event.clientY;     // Get the vertical coordinate
+                did++;
 
-    })
+
+                color = $('.jscolor').val();
+                size = $('#custom-handle').html();
+
+                currentElement[did] = {"x": x, "y":y, "bgcolor":color, "size":size};
+
+                $("#canvas").append("<div class='circle' style='position: absolute; top: "+(y- 130)+"px; left: "+(x-310)+"px; background-color:"+color+"; border-radius: 50px; width: "+size+"px; height: "+size+"px'></div>");
+            }
+        })
+        .mousedown(function() {
+            moving = true;
+            beid++;
+            did = 0;
+            currentElement = {};
+        })
+        .mouseup(function() {
+            moving = false;
+            console.log(beid);
+            brushElements[beid] = currentElement;
+            console.log(brushElements);
+        });
+    // --------------------------------/PAINTBRUSH------------------------------------------
 
 
+
+
+    // -----------------------------------BUCKET--------------------------------------------
+
+
+    // ----------------------------------/BUCKET--------------------------------------------
 });
